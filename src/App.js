64c1436eea462b4f038e95f400bf8acb8ea32bd0
saams4u <mahjouri.saamahn@gmail.com>
2022-01-08 20:46:11 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+
 import React, { useEffect, useState, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { connect } from "./redux/blockchain/blockchainActions";
@@ -114,7 +115,7 @@ function App() {
             <s.TextTitle
               style={{ textAlign: "center", fontSize: 26, fontWeight: "bold" }}
             >
-              {data.totalSupply}/10000
+              {data.totalSupply}/5000
             </s.TextTitle>
           </s.Container>
           <s.SpacerMedium />
@@ -124,7 +125,7 @@ function App() {
             ai={"center"}
             style={{ backgroundColor: "#000000", padding: 12 }}
           >
-            {Number(data.totalSupply) == 10000 ? (
+            {Number(data.totalSupply) == 5000 ? (
               <>
                 <s.TextTitle style={{ textAlign: "center" }}>
                   The sale has ended.
